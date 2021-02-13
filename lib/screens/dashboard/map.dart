55def258e90b19f60fade6e184d5ff8bb791a7ba
Patrick <patrick@patrickjoy.com>
2021-02-13 20:01:44 +1100

    Fixed bug preventing apk build

--- lib/screens/dashboard/map.dart
+++ lib/screens/dashboard/map.dart
@@ -4,6 +4,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
 import '../../models/moto.dart';
 import '../../utils/service_locator.dart' as SL;
+import 'package:flutter_map/plugin_api.dart';
 
 class Map extends StatefulWidget {
   @override
@@ -59,7 +60,7 @@ class _MapState extends State<Map> with AutomaticKeepAliveClientMixin<Map> {
                         // For example purposes. It is recommended to use
                         // TileProvider with a caching and retry strategy, like
                         // NetworkTileProvider or CachedNetworkTileProvider
-                        tileProvider: CachedNetworkTileProvider(),
+                        //tileProvider: CachedNetworkTileProvider(),
                       ),
                       CircleLayerOptions(circles: moto.markers)
                     ],
